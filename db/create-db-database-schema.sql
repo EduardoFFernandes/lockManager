96d75bccb459d90851ead32e857388475f96d2ce
Mateus <bittenmat@gmail.com>
2022-07-07 14:13:59 -0300

    db commands updated

--- db/create-db-database-schema.sql
+++ db/create-db-database-schema.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE dblockmanager_desenv
     WITH 
-    OWNER = lockmanager
+    OWNER = dblockmanager
     ENCODING = 'UTF8'
     LC_COLLATE = 'en_US.UTF-8'
     LC_CTYPE = 'en_US.UTF-8'
